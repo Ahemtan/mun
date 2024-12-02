86ada8c20341e40d502b1e7353ebb2c61d932e88
Ahem <87135144+Ahemtan@users.noreply.github.com>
2024-12-02 21:03:43 +0545

    Basic Home Page

--- client/src/components/ui/Navbar.jsx
+++ client/src/components/ui/Navbar.jsx
@@ -10,19 +10,19 @@ const Navbar = () => {
     const { user, logout } = useUserStore();
 
     return (
-        <header className="bg-white">
+        <header className="bg-white w-screen fixed">
             <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                     <div className="md:flex md:items-center md:gap-12">
-                        <a className="block text-teal-600" href="#">
+                        <Link to={"/"} className="block text-teal-600" href="#">
                             <span className="sr-only">Home</span>
                             <img width={64} height={64} src="/logo.svg" alt='logo' />
-                        </a>
+                        </Link>
                     </div>
 
                     <div className="hidden md:block">
                         <nav aria-label="Global">
-                            <ul className="flex items-center gap-6 text-sm">
+                            <ul className="flex items-center gap-6 text-lg">
                                 <li>
                                     <Link className="text-gray-500 transition hover:text-gray-500/75" href="#"> About </Link>
                                 </li>
@@ -86,18 +86,22 @@ const Navbar = () => {
                         <div className="sm:flex sm:gap-4">
 
                             {
-                                user ? <button
-                                    onClick={logout}
-                                    className="rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-                                    href="#"
-                                >
-                                    <LogOut className='size-5' />
-                                </button> : <a
+                                user ? <Link
+                                    to={'/auth/login'}
                                     className="rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white shadow"
                                     href="#"
                                 >
                                     Login
-                                </a>
+                                </Link>
+
+                                    :
+                                    <button
+                                        onClick={logout}
+                                        className="rounded-md bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+                                        href="#"
+                                    >
+                                        <LogOut className='size-5' />
+                                    </button>
                             }
 
                         </div>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,23 +1,81 @@
-import React, { useEffect } from 'react'
-import { useUserStore } from '../store/useUserStore';
+import React from 'react'
 
 const HomePage = () => {
-
-  const { checkAuth, user, logout } = useUserStore();
-
-  useEffect(() => {
-      checkAuth();
-  }, [])
 
   return (
     <div className='text-xl'>
 
-      {
-        user ? <button onClick={logout}>Logout</button> : "login now!"
-      }
+      <section className="bg-gray-50">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto max-w-xl text-center">
+            <h1 className="text-3xl font-extrabold sm:text-5xl">
+              Understand User Flow.
+              <strong className="font-extrabold text-indigo-700 sm:block"> Increase Conversion. </strong>
+            </h1>
 
-      <h1>Home</h1>
-      
+            <p className="mt-4 sm:text-xl/relaxed">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo tenetur fuga ducimus
+              numquam ea!
+            </p>
+
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
+              <a
+                className="block w-full rounded bg-indigo-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-indigo-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
+                href="#"
+              >
+                Get Started
+              </a>
+
+              <a
+                className="block w-full rounded px-12 py-3 text-sm font-medium text-indigo-600 shadow hover:text-indigo-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
+                href="#"
+              >
+                Learn More
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+        <div className="mx-auto max-w-3xl text-center">
+          <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">Number Dosen't Lie</h2>
+
+          <p className="mt-4 text-gray-500 sm:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolores laborum labore
+            provident impedit esse recusandae facere libero harum sequi.
+          </p>
+        </div>
+
+        <dl
+          className="mg-6 grid grid-cols-1 gap-4 divide-y divide-gray-100 sm:mt-8 sm:grid-cols-2 sm:divide-x sm:divide-y-0 lg:grid-cols-4"
+        >
+          <div className="flex flex-col px-4 py-8 text-center">
+            <dt className="order-last text-lg font-medium text-gray-500">Total Sales</dt>
+
+            <dd className="text-4xl font-extrabold text-indigo-600 md:text-5xl">$4.8m</dd>
+          </div>
+
+          <div className="flex flex-col px-4 py-8 text-center">
+            <dt className="order-last text-lg font-medium text-gray-500">Official Addons</dt>
+
+            <dd className="text-4xl font-extrabold text-indigo-600 md:text-5xl">24</dd>
+          </div>
+
+          <div className="flex flex-col px-4 py-8 text-center">
+            <dt className="order-last text-lg font-medium text-gray-500">Total Addons</dt>
+
+            <dd className="text-4xl font-extrabold text-indigo-600 md:text-5xl">86</dd>
+          </div>
+
+          <div className="flex flex-col px-4 py-8 text-center">
+            <dt className="order-last text-lg font-medium text-gray-500">Downloads</dt>
+
+            <dd className="text-4xl font-extrabold text-indigo-600 md:text-5xl">86k</dd>
+          </div>
+        </dl>
+      </div>
+
     </div>
   )
 }
